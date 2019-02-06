8bb290e8e182575ccafaf4c8a3296500a39682d9
elenia3103 <go_lenka12357@mail.ru>
2019-02-06 17:27:45 +0300

    Метод distance реализован в исполняемом классе

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -6,7 +6,12 @@ public class MyFirstProg {
     public static void main (String[] args) {
         Point p1 = new Point(10, 3);
         Point p2 = new Point(4, 6);
-        System.out.println("Расстояние между двумя точками p1 и p2 = "+p1.distance(p1,p2));
+        System.out.println("Расстояние между двумя точками p1 и p2 = "+distance(p1,p2));
+    }
+    public static double distance (Point p1, Point p2) {
+        double a = Math.pow((p1.x-p2.x),2);
+        double b = Math.pow((p1.y-p2.y),2);
+        return Math.sqrt(a+b);
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,12 +7,4 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
-    public double distance (Point p1, Point p2) {
-        double a = Math.pow((p1.x-p2.x),2);
-        double b = Math.pow((p1.y-p2.y),2);
-        return Math.sqrt(a+b);
-    }
-
-
 }
