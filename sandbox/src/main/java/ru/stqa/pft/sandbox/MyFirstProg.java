978fcef1febe8c21f515c9095b7ca00614a49720
elenia3103 <go_lenka12357@mail.ru>
2019-02-08 15:15:39 +0300

    Функция вычисления расстояния реализована в классе Point (исправленное)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProg.java
@@ -6,11 +6,6 @@ public class MyFirstProg {
     public static void main (String[] args) {
         Point p1 = new Point(10, 3);
         Point p2 = new Point(4, 6);
-        System.out.println("Расстояние между двумя точками p1 и p2 = "+distance(p1, p2));
+        System.out.println("Расстояние между двумя точками p1 и p2 = "+p1.distance(p2));
     }
-    public static double distance (Point p1, Point p2) {
-
-        return Math.sqrt(Math.pow((p1.x-p2.x),2)+Math.pow((p1.y-p2.y),2));
-    }
-
 }
