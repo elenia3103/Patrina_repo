c0c3e601afa6e82b00db10fb4bf406786ecd2d10
elenia3103 <go_lenka12357@mail.ru>
2019-02-13 14:39:04 +0300

    Проведен рефакторинг кода тестов создания новой группы SecondGroupCreationTests
    Выделены вспомогательные методы, в том числе метод заполнения формы.
    Параметризован метод заполнения формы.
    Сгруппированы параметры метода заполнения формы в объект, сделан вспомогательный "модельный" класс GroupData.

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/SecondGroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/SecondGroupCreationTests.java
@@ -14,31 +14,51 @@ public class SecondGroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testSecondGroupCreation() throws Exception {
-    wd.findElement(By.name("new")).click();
+    initGroupCreation();
+    fillGroupForm(new GroupData("testov", "test", "testovodze"));
+    submitGroupCreation();
+    returnToGroupPage();
+
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("testov");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Groups'])[1]/following::form[1]")).click();
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("testovodze");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
 
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
