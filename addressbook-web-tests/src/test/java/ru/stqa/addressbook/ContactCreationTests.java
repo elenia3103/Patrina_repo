4091b99c163b26e6a65dc8fce3e924da8aeecd84
elenia3103 <go_lenka12357@mail.ru>
2019-02-13 15:06:13 +0300

    ContactCreationTests: код ввода логина/пароля перенесен в метод инициализации фикстуры

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactCreationTests.java
@@ -13,10 +13,6 @@ public class ContactCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testContactCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -24,6 +20,11 @@ public class ContactCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void testContactCreation() throws Exception {
+
     wd.findElement(By.linkText("add new")).click();
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
