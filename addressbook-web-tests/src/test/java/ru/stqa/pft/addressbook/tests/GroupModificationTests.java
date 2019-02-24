77d2fee81bea4ded59542a89671dece9121af92c
elenia3103 <go_lenka12357@mail.ru>
2019-02-24 18:13:56 +0300

    Добавлен класс модификации групп GroupModificationTests, созданы дополнительные вспомогательные методы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+    @Test
+    public void testGroupModification(){
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("testov", "test", "testovodze"));
+        app.getGroupHelper().submitGroupModification();
+        app.getNavigationHelper().returnToGroupPage();
+    }
+}
