185a270926d435d1bc7b680f8ce833809d1cd9c5
elenia3103 <go_lenka12357@mail.ru>
2019-03-25 16:26:26 +0300

    Параметризованы тесты создания групп и контактов
    Для групп данные читаются из файла формата xml
    Для контактов данные читаются из файла формата json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,10 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,13 +21,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.json"))));
+    String json ="";
+    String line = reader.readLine();
+    while (line !=null) {
+      json+=line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((g) ->new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test (dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) throws Exception {
     app.goTo().contactPage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("Alexey").withLastname("Host").withNickname("nex").withCompany("Net")
-            .withHome("8888").withMobile("9877645").withAddress("Yrupinsk").withEmail("111@ff").withPhoto(photo);
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size()+1));
     Contacts after = app.contact().all();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SecondGroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SecondGroupCreationTests.java
@@ -1,20 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SecondGroupCreationTests extends TestBase {
 
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xsteram = new XStream();
+    xsteram.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xsteram.fromXML(xml);
+    return groups.stream().map((g) ->new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
 
-  @Test
-  public void testSecondGroupCreation() throws Exception {
+  @Test(dataProvider = "validGroupsFromXml")
+  public void testSecondGroupCreation(GroupData group) throws Exception {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("testovyi").withHeader("test");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()+1));
     Groups after = app.group().all();
