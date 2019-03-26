a00b4a259fa477f194d112f4abb620acf1a95f14
elenia3103 <go_lenka12357@mail.ru>
2019-03-26 08:41:43 +0300

    Реализовано автоматическое закрытие файлов для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SecondGroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/SecondGroupCreationTests.java
@@ -20,17 +20,18 @@ public class SecondGroupCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
+    try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))){
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xsteram = new XStream();
+      xsteram.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xsteram.fromXML(xml);
+      return groups.stream().map((g) ->new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xsteram = new XStream();
-    xsteram.processAnnotations(GroupData.class);
-    List<GroupData> groups = (List<GroupData>) xsteram.fromXML(xml);
-    return groups.stream().map((g) ->new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validGroupsFromXml")
